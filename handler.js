a6fa2c02ec82922f60c8519a9862a8cabf3dd6e3
Tam Pham <tampham47@live.com>
2020-05-09 22:29:07 +0700

    add information to deploy on aws

--- handler.js
+++ handler.js
@@ -1,11 +1,21 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
+module.exports.message = async (event) => {
+  const body = JSON.parse(event.body);
+  console.log('body', body);
+
+  if (body.type === 'url_verification') {
+    return {
+      statusCode: 200,
+      body: body.challenge,
+    }
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Go Serverless v1.0! Your function executed successfully!",
         input: event,
       },
       null,
