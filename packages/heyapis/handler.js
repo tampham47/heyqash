df661fffbe90da0e8d7851f1b16af55aab5d116a
Tam Pham <tampham47@live.com>
2020-05-30 23:30:17 +0700

    add more apis: messages and transactions

--- packages/heyapis/handler.js
+++ packages/heyapis/handler.js
@@ -1,26 +1,72 @@
 'use strict';
-const { Logs } = require('@heyliquid/shared/collections');
+const {
+  Logs,
+  Messages,
+  Transactions,
+} = require('@heyliquid/shared/collections');
 const apiResult = require('@heyliquid/shared/apiResults');
 
-module.exports.logs = async () => {
-  const logs = await Logs.orderBy('timestamp', 'desc')
-    .limit(10)
+const MAX_ITEM = 100;
+
+const getOptions = (event) => {
+  const params = event.queryStringParameters || {};
+  const skip = parseInt(params.skip || '0');
+  const limit = parseInt(params.limit || '10');
+
+  return {
+    skip,
+    limit,
+  };
+};
+
+const getData = (Model, opts) => {
+  return Model.orderBy('timestamp', 'desc')
+    .limit(opts.limit > MAX_ITEM ? MAX_ITEM : opts.limit)
+    .offset(opts.skip)
     .get()
     .then((snapshot) => {
       if (snapshot.empty) {
         return [];
       }
-
       const data = [];
       snapshot.forEach((doc) => {
         data.push(doc.data());
       });
-
       return data;
-    })
-    .catch((err) => {
-      console.log('Error getting documents', err);
+    });
+};
+
+module.exports.logs = async (event) => {
+  console.log('event', event);
+
+  const options = getOptions(event);
+  const data = await getData(Logs, options).catch((error) => {
+    return apiResult(500, { error });
+  });
+
+  return apiResult(200, { data });
+};
+
+module.exports.messages = async (event) => {
+  console.log('event', event);
+
+  const options = getOptions(event);
+  const data = await getData(Messages, options).catch((error) => {
+    return apiResult(500, { error });
+  });
+
+  return apiResult(200, { data });
+};
+
+module.exports.transactions = async (event) => {
+  console.log('event', event);
+
+  const options = getOptions(event);
+  const data = await getData(Transactions, options)
+    .then((d) => d.map((i) => ({ ...i, _user: null, _raw: null })))
+    .catch((error) => {
+      return apiResult(500, { error });
     });
 
-  return apiResult(200, { data: logs });
+  return apiResult(200, { data });
 };
--- packages/shared/collections.js
+++ packages/shared/collections.js
@@ -5,9 +5,13 @@ const db = firebase.firestore();
 const Logs = db.collection('logs');
 const Messages = db.collection('messages');
 const Transactions = db.collection('transactions');
+const Users = db.collection('users');
+const Reports = db.collection('reports');
 
 module.exports = {
   Logs,
   Messages,
   Transactions,
+  Users,
+  Reports,
 };
